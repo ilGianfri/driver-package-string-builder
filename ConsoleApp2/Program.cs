5721f8b49b775348258fef0a801ab759d2ffabac
Alessandro Spisso <5341363+ilGianfri@users.noreply.github.com>
2021-01-22 17:36:38 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] fileEntries = Directory.GetDirectories(@"C:\10X\DCHUDrivers");
+
+            using StreamWriter w = File.AppendText("output.txt");
+            {
+                foreach (string directory in fileEntries)
+                {
+                    string[] files = Directory.GetFiles(directory).Where(x => Path.GetExtension(x) == ".inf").ToArray();
+                    foreach (string file in files)
+                    {
+                        w.WriteLine("<DriverPackageFile Path=\"" + directory + "\"" + $" Name=\"{Path.GetFileName(file)}\"/>");
+                    }
+                }
+            }
+        }
+    }
+}
